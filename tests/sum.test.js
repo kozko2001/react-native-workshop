e13b9027ead2979fe2776a24954b7778589e7998
Jordi Coscolla <jcoscol@thoughtworks.com>
2018-09-26 11:26:19 +0200

    ADD eslint

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,9 +1,9 @@
 
 describe('sum operation', () => {
   it('2+2 == 4', () => {
-    const actual = 2 + 2;
-    const result = 4;
+    const actual = 2 + 2
+    const result = 4
 
-    expect(actual).toEqual(result);
-  });
-});
+    expect(actual).toEqual(result)
+  })
+})
